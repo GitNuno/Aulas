3d47cdad0957c137c01ff3fd930630b7b0e891c4
Nuno <yo_325@yahoo.com>
2017-11-13 14:23:25 +0000

    Teste_2 : using Microsoft.EntityFrameworkCore;

--- Apagar/Apagar/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ Apagar/Apagar/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -334,7 +334,7 @@ if (typeof jQuery === 'undefined') {
   Carousel.TRANSITION_DURATION = 600
 
   Carousel.DEFAULTS = {
-    interval: 51592,
+    interval: 53159,
     pause: 'hover',
     wrap: true,
     keyboard: true
