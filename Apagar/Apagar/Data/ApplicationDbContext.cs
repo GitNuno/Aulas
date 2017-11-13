f2a55bf467e82a138ff64d2c297e53869227ecf3
Nuno <yo_325@yahoo.com>
2017-11-13 14:29:19 +0000

    Revert "Instalado: using Microsoft.EntityFrameworkCore;"
    This reverts commit 293fa91267f670d92dc12cc4d9f2aed37e5721f0.

--- Apagar/Apagar/Data/ApplicationDbContext.cs
+++ Apagar/Apagar/Data/ApplicationDbContext.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Apagar.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext
     {
     }
 }
